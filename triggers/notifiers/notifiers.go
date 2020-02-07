cb570590fff9dc1f3cb97a1932fff8c02b6d6bb7
Huy Ngo <huynvk@codelink.io>
2020-02-07 14:15:19 +0700

    Add simple trigger

---
+++ triggers/notifiers/notifiers.go
@@ -0,0 +1,30 @@
+package notifiers
+
+import (
+	"goworkers/tasks"
+	"goworkers/triggers"
+)
+
+type notifier struct {
+	tasks  chan tasks.Task
+	errors chan error
+}
+
+func NewTrigger() triggers.Trigger {
+	return &notifier{
+		tasks:  make(chan tasks.Task),
+		errors: make(chan error),
+	}
+}
+
+func (t *notifier) Listen() chan tasks.Task {
+	return t.tasks
+}
+
+func (t *notifier) NotifyError() chan error {
+	return t.errors
+}
+
+func (t *notifier) Notify(task tasks.Task) {
+	t.tasks <- task
+}
